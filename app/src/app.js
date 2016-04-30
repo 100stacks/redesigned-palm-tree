fab9faabd48fc95cd6671f4a0805df9242910e6d
100stacks <100stacks@users.noreply.github.com>
2016-04-30 17:18:45 -0500

    initial Angular module commit

---
+++ app/src/app.js
@@ -0,0 +1,31 @@
+/*
+ * app.js
+ *
+ * Contains the angular logic for the app.
+ */ 
+angular.module('contactsApp', ['ngRoute'])
+    .config(function($routeProvider)) {
+        $routeProvider
+        .when('/', {
+            templateUrl: 'list.html',
+            controller: 'ListController',
+            resolve: {
+                contacts: function(Contacts) {
+                    return Contacts.getContacts();
+                }
+            }
+        })
+    }
+    .service('Contacts', function($http) {
+        this.getContacts = function() {
+            return $http.get('/contacts').
+                then(function (response) {
+                    return response;
+                }), function (response) {
+                    alert('Error retrieving contacts.');
+                }
+        }
+    })
+    .controller('ListController', function(contacts, $scope) {
+        $scope.contacts = contacts.data;
+    });
