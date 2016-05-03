28f38930cc4ba7420683584291e5849dbefab781
100stacks <100stacks@users.noreply.github.com>
2016-05-03 00:25:42 -0500

    moving services and controllers to their own files

--- app/src/app.js
+++ app/src/app.js
@@ -4,6 +4,7 @@
  * Contains the angular logic for the app.
  */ 
 angular.module('contactsApp', ['ngRoute'])
+
     .config(function($routeProvider)) {
         $routeProvider
         .when('/', {
@@ -26,17 +27,4 @@ angular.module('contactsApp', ['ngRoute'])
     })
     .otherwise({
         redirectTo: '/'
-    })
-    .service('Contacts', function($http) {
-        this.getContacts = function() {
-            return $http.get('/contacts').
-                then(function (response) {
-                    return response;
-                }), function (response) {
-                    alert('Error retrieving contacts.');
-                }
-        }
-    })
-    .controller('ListController', function(contacts, $scope) {
-        $scope.contacts = contacts.data;
     });
