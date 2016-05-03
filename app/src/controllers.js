5493a699edaaf7ea1d62c48c87c9667e91d90529
100stacks <100stacks@users.noreply.github.com>
2016-05-03 00:57:40 -0500

    adding edit contact controller

--- app/src/controllers.js
+++ app/src/controllers.js
@@ -4,18 +4,46 @@ angular.module('contactsApp')
         $scope.contacts = contacts.data;
     })
     .controller('NewContactController', function($scope, $location, Contacts) {
-    	$scope.back = function () {
-    		$location.path('#/');
-    	}
+        $scope.back = function () {
+            $location.path('#/');
+        }
 
-    	$scope.saveContact = function (contact) {
-    		Contacts.createContact(contact).
-    			then(function (doc) {
-    				var contactUrl = '/contact' + doc.data._id;
-    				$location.path(contactUrl);
-    			}, function (response) {
-    				alert(response);
-    			});
-    	}
+        $scope.saveContact = function (contact) {
+            Contacts.createContact(contact).
+                then(function (doc) {
+                    var contactUrl = '/contact' + doc.data._id;
+                    $location.path(contactUrl);
+                }, function (response) {
+                    alert(response);
+                });
+        }
     })
-    
+    .controller('EditContactController', function($scope, $routeParams, Contacts) {
+        Contacts.getContact($routeParams.contactId).
+            then(function (doc) {
+                $scope.contact = doc.data;
+            }, function (response) {
+                alert(response);
+            });
+
+        $scope.toggleEdit = function () {
+            $scope.editMode = true;
+            $scope.contactFormUrl = 'contact-form.html';
+        }
+
+        $scope.back = function () {
+            $scope.editMode = false;
+            $scope.contactFormUrl = '';
+        }
+
+        $scope.saveContact = function (contact) {
+            Contacts.editContact(contact);
+            $scope.editMode = false;
+            $scope.contactFormUrl = '';
+        }
+
+        $scope.deleteContact = function (contactId) {
+            Contacts.deleteContact(contactId);
+        }
+    });
+
